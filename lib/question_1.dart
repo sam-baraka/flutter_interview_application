1a4abf8330a53257cec5f62d9dfaca9fb8be3baa
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-02-06 21:29:59 +0300

    Added a tab controller of length 2

--- lib/question_1.dart
+++ lib/question_1.dart
@@ -3,102 +3,107 @@ import 'package:flutter/material.dart';
 class Question1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Positioned(
-          top: 0,
-          left: 0,
-          right: 0,
-          child: Image.asset(
-            'assets/woman_balck.jpg',
-            fit: BoxFit.fitWidth,
+    return DefaultTabController(
+      length: 2,
+      child: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Image.asset(
+              'assets/woman_balck.jpg',
+              fit: BoxFit.fitWidth,
+            ),
           ),
-        ),
-        Scaffold(
-          backgroundColor: Colors.black54,
-          body: Stack(
-            children: [
-              Positioned(
+          Scaffold(
+            backgroundColor: Colors.black54,
+            body: Stack(
+              children: [
+                Positioned(
+                    top: 40,
+                    right: 20,
+                    child: Icon(
+                      Icons.dehaze,
+                      color: Colors.white,
+                    )),
+                Positioned(
+                  left: 0,
                   top: 40,
-                  right: 20,
-                  child: Icon(
-                    Icons.dehaze,
-                    color: Colors.white,
-                  )),
-              Positioned(
-                left: 0,
-                top: 40,
-                right: 0,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 20, bottom: 20),
-                      child: Text(
-                        "Hello,\nAnne Kibe",
-                        style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 20, right: 40),
-                      child: Row(
-                        children: [
-                          Text(
-                            "KSH 135.50",
-                            style: TextStyle(fontSize: 16, color: Colors.white),
-                          ),
-                          Expanded(
-                            child: Container(),
-                          ),
-                          Text(
-                            "KSH 2,360.30",
-                            style: TextStyle(fontSize: 16, color: Colors.white),
-                          )
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          left: 20, right: 40, bottom: 20),
-                      child: Row(
-                        children: [
-                          Text(
-                            "Today's Earnings",
-                            style: TextStyle(
-                                fontSize: 14, color: Colors.lightBlue),
-                          ),
-                          Expanded(
-                            child: Container(),
-                          ),
-                          Text(
-                            "Total Earnings",
-                            style: TextStyle(
-                                fontSize: 14, color: Colors.lightBlue),
-                          )
-                        ],
-                      ),
-                    ),
-                    Container(
-                      height: MediaQuery.of(context).size.height,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          topRight: Radius.circular(20),
+                  right: 0,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20, bottom: 20),
+                        child: Text(
+                          "Hello,\nAnne Kibe",
+                          style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
                         ),
                       ),
-                    )
-                  ],
-                ),
-              )
-            ],
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20, right: 40),
+                        child: Row(
+                          children: [
+                            Text(
+                              "KSH 135.50",
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.white),
+                            ),
+                            Expanded(
+                              child: Container(),
+                            ),
+                            Text(
+                              "KSH 2,360.30",
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.white),
+                            )
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            left: 20, right: 40, bottom: 20),
+                        child: Row(
+                          children: [
+                            Text(
+                              "Today's Earnings",
+                              style: TextStyle(
+                                  fontSize: 14, color: Colors.lightBlue),
+                            ),
+                            Expanded(
+                              child: Container(),
+                            ),
+                            Text(
+                              "Total Earnings",
+                              style: TextStyle(
+                                  fontSize: 14, color: Colors.lightBlue),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: MediaQuery.of(context).size.height,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20),
+                            topRight: Radius.circular(20),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
