687e9a634a7e4bda68cc4b41bea612e6d7a0092d
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-02-06 22:09:58 +0300

    WIdget abstracion

--- lib/1/widgets/friends_widget.dart
+++ lib/1/widgets/friends_widget.dart
@@ -1,8 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class FriendsWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: [
+        FriendCard(),
+        FriendCard(),
+        FriendCard(),
+        FriendCard(),
+        FriendCard(),
+      ],
+    );
+  }
+}
+
+class FriendCard extends StatelessWidget {
+  final String image;
+  final String name;
+
+  const FriendCard({Key key, this.image, this.name}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: 120,
+        // decoration: BoxDecoration(borderRadius: BorderRadius.circular(5)),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10.0),
+              child: Image.asset(
+                'assets/woman_balck.jpg',
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            Text("Sharon")
+          ],
+        ),
+      ),
+    );
   }
 }
