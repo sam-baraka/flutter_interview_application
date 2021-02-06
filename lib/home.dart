f7cbf7849a5ab5c07da31007851735d9addc3b06
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-02-06 20:44:22 +0300

    Implemented home page

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,35 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+                "Click on the buttons bellow to view my answer for the respective question"),
+            CupertinoButton(
+              onPressed: () {},
+              color: Colors.blue,
+              child: Text("Question 1"),
+            ),
+            CupertinoButton(
+              onPressed: () {},
+              color: Colors.blue,
+              child: Text("Question 2"),
+            ),
+            CupertinoButton(
+              onPressed: () {},
+              color: Colors.blue,
+              child: Text("Question 3"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
