8496e493e8e0fc22e525a292c361a7753860a8bc
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-02-08 23:09:27 +0300

    Implemented basic UI Structure

--- lib/3/question_3.dart
+++ lib/3/question_3.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_interview_test/3/widgets/chart_widget.dart';
 
 class Question3 extends StatelessWidget {
   @override
@@ -8,8 +9,39 @@ class Question3 extends StatelessWidget {
       appBar: CupertinoNavigationBar(
         middle: Text("Question 3"),
       ),
-      body: Column(
-        children: [],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(child: Text("Men")),
+                SizedBox(
+                  width: 20,
+                ),
+                Expanded(child: Text("Women"))
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(border: OutlineInputBorder()),
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(border: OutlineInputBorder()),
+                  ),
+                ),
+              ],
+            ),
+            ChartWidget()
+          ],
+        ),
       ),
     );
   }
