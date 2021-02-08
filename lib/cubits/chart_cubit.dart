9118c6c7066e17a82ee3628aa3e7d7be513d7907
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-02-08 23:00:45 +0300

    Added parameters

--- lib/cubits/chart_cubit.dart
+++ lib/cubits/chart_cubit.dart
@@ -2,9 +2,16 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ChartCubit extends Cubit<ChartDetails> {
   ChartCubit(ChartDetails state) : super(state);
+  update({int male, int female}) {
+    emit(ChartDetails(male, female));
+  }
 }
 
 class ChartDetails {
-  static int male;
-  static int female;
+  int male;
+  int female;
+  ChartDetails(
+    this.male,
+    this.female,
+  );
 }
