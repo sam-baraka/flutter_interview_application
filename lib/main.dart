fb3238a470977e547e3bd1bd76ffcc8839d6bca3
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-02-08 12:04:05 +0300

    Added select team bloc provider and simple bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_interview_test/cubits/select_team_cubit.dart';
 import 'package:flutter_interview_test/home.dart';
 
-void main() {
+import 'blocs/simple_bloc_observer.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = SimpleBlocObserver();
   runApp(InterviewApp());
 }
 
@@ -10,6 +15,9 @@ class InterviewApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
+      providers: [
+        BlocProvider<SelectTeamCubit>(create: (context) => SelectTeamCubit(0)),
+      ],
       child: MaterialApp(
         home: Home(),
       ),
