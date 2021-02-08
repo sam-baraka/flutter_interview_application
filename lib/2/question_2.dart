5da49ef3c29142d8ef4c685a6c4ae078b7a9c632
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-02-08 11:26:17 +0300

    Added question 2 file

---
+++ lib/2/question_2.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Question2 extends StatefulWidget {
+  @override
+  _Question2State createState() => _Question2State();
+}
+
+class _Question2State extends State<Question2> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
