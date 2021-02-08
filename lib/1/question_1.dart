18e52a8833aedf56cf9fbf78e8c11d5e13d70f59
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-02-08 11:24:51 +0300

    Added discover widget to question 1 page

--- lib/1/question_1.dart
+++ lib/1/question_1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_interview_test/1/widgets/discover_widget.dart';
 import 'package:flutter_interview_test/1/widgets/friends_widget.dart';
 
 import 'widgets/community_widget.dart';
@@ -34,107 +35,114 @@ class Question1 extends StatelessWidget {
                   left: 0,
                   top: 40,
                   right: 0,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 20, bottom: 20),
-                        child: Text(
-                          "Hello,\nAnne Kibe",
-                          style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 20, right: 40),
-                        child: Row(
-                          children: [
-                            Text(
-                              "KSH 135.50",
-                              style:
-                                  TextStyle(fontSize: 16, color: Colors.white),
-                            ),
-                            Expanded(
-                              child: Container(),
-                            ),
-                            Text(
-                              "KSH 2,360.30",
-                              style:
-                                  TextStyle(fontSize: 16, color: Colors.white),
-                            )
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(
-                            left: 20, right: 40, bottom: 20),
-                        child: Row(
-                          children: [
-                            Text(
-                              "Today's Earnings",
-                              style: TextStyle(
-                                  fontSize: 14, color: Colors.lightBlue),
-                            ),
-                            Expanded(
-                              child: Container(),
-                            ),
-                            Text(
-                              "Total Earnings",
-                              style: TextStyle(
-                                  fontSize: 14, color: Colors.lightBlue),
-                            )
-                          ],
-                        ),
-                      ),
-                      Container(
-                        height: MediaQuery.of(context).size.height,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(20),
-                            topRight: Radius.circular(20),
+                  bottom: 0,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 20, bottom: 20),
+                          child: Text(
+                            "Hello,\nAnne Kibe",
+                            style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
                           ),
                         ),
-                        child: ListView(
-                          children: [
-                            TabBar(
-                              indicatorSize: TabBarIndicatorSize.label,
-                              indicatorColor: Colors.white,
-                              labelStyle: TextStyle(
-                                  fontSize: 14, fontWeight: FontWeight.bold),
-                              labelColor: Colors.black,
-                              unselectedLabelColor: Colors.grey,
-                              tabs: [
-                                Row(
+                        Padding(
+                          padding: const EdgeInsets.only(left: 20, right: 40),
+                          child: Row(
+                            children: [
+                              Text(
+                                "KSH 135.50",
+                                style: TextStyle(
+                                    fontSize: 16, color: Colors.white),
+                              ),
+                              Expanded(
+                                child: Container(),
+                              ),
+                              Text(
+                                "KSH 2,360.30",
+                                style: TextStyle(
+                                    fontSize: 16, color: Colors.white),
+                              )
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: 20, right: 40, bottom: 20),
+                          child: Row(
+                            children: [
+                              Text(
+                                "Today's Earnings",
+                                style: TextStyle(
+                                    fontSize: 14, color: Colors.lightBlue),
+                              ),
+                              Expanded(
+                                child: Container(),
+                              ),
+                              Text(
+                                "Total Earnings",
+                                style: TextStyle(
+                                    fontSize: 14, color: Colors.lightBlue),
+                              )
+                            ],
+                          ),
+                        ),
+                        Container(
+                          height: MediaQuery.of(context).size.height,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(20),
+                              topRight: Radius.circular(20),
+                            ),
+                          ),
+                          child: ListView(
+                            children: [
+                              TabBar(
+                                indicatorSize: TabBarIndicatorSize.label,
+                                indicatorColor: Colors.white,
+                                labelStyle: TextStyle(
+                                    fontSize: 14, fontWeight: FontWeight.bold),
+                                labelColor: Colors.black,
+                                unselectedLabelColor: Colors.grey,
+                                tabs: [
+                                  Row(
+                                    children: [
+                                      Text(
+                                        "Friends",
+                                        textAlign: TextAlign.left,
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text(
+                                        "Community",
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                              Container(
+                                height: 130,
+                                child: TabBarView(
                                   children: [
-                                    Text(
-                                      "Friends",
-                                      textAlign: TextAlign.left,
-                                    ),
+                                    FriendsWidget(),
+                                    CommunityWidget()
                                   ],
                                 ),
-                                Row(
-                                  children: [
-                                    Text(
-                                      "Community",
-                                    ),
-                                  ],
-                                )
-                              ],
-                            ),
-                            Container(
-                              height: 130,
-                              child: TabBarView(
-                                children: [FriendsWidget(), CommunityWidget()],
                               ),
-                            )
-                          ],
+                              DiscoverWidget()
+                            ],
+                          ),
                         ),
-                      )
-                    ],
+                      ],
+                    ),
                   ),
                 )
               ],
