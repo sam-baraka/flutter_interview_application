fa427553d4ff70db990a059de35dea20be6aef26
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-02-08 23:22:50 +0300

    Added text styles

--- lib/3/question_3.dart
+++ lib/3/question_3.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_interview_test/3/widgets/chart_widget.dart';
 
 class Question3 extends StatelessWidget {
+  TextEditingController menController = TextEditingController();
+  TextEditingController womenController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,20 +14,34 @@ class Question3 extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Row(
               children: [
-                Expanded(child: Text("Men")),
+                Expanded(
+                    child: Text(
+                  "Men",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold),
+                )),
                 SizedBox(
                   width: 20,
                 ),
-                Expanded(child: Text("Women"))
+                Expanded(
+                    child: Text("Women",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold)))
               ],
             ),
             Row(
               children: [
                 Expanded(
                   child: TextFormField(
+                    controller: menController,
                     decoration: InputDecoration(border: OutlineInputBorder()),
                   ),
                 ),
@@ -34,6 +50,7 @@ class Question3 extends StatelessWidget {
                 ),
                 Expanded(
                   child: TextFormField(
+                    controller: womenController,
                     decoration: InputDecoration(border: OutlineInputBorder()),
                   ),
                 ),
